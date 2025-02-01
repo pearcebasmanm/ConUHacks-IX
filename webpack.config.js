8f2513553d3713974c6feac9d1334fcf373ca5b9
Max <pearcebasmanm@gmail.com>
2025-02-01 14:06:22 -0500

    refine options page

--- webpack.config.js
+++ webpack.config.js
@@ -7,22 +7,23 @@ module.exports = {
     config: "./src/config.js",
     content: "./src/content.js",
     options: "./src/options.js",
-    popup: "./src/popup.js"
+    popup: "./src/popup.js",
   },
   output: {
     filename: "[name].js", // Produces dist/background.js, dist/content.js, etc.
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
   },
   resolve: {
-    extensions: [".js"]
+    extensions: [".js"],
   },
   plugins: [
     new CopyPlugin({
       patterns: [
         { from: "manifest.json", to: "manifest.json" },
         { from: "src/popup.html", to: "popup.html" },
-        { from: "src/options.html", to: "options.html" }
-      ]
-    })
-  ]
+        { from: "src/options.html", to: "options.html" },
+        { from: "src/options.css", to: "options.css" },
+      ],
+    }),
+  ],
 };
