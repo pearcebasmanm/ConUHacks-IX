1b54f111c5a8b426e9644b449f00000b5fe26357
Artypuppet <jaspreetsingh8437055402@gmail.com>
2025-02-01 12:49:02 -0500

    Restructured the project and added webpack to build it. Also added gemini's sdk to make requests.

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: {
+    background: "./src/background.js",
+    config: "./src/config.js",
+    content: "./src/content.js",
+    options: "./src/options.js",
+    popup: "./src/popup.js"
+  },
+  output: {
+    filename: "[name].js", // Produces dist/background.js, dist/content.js, etc.
+    path: path.resolve(__dirname, "dist")
+  },
+  resolve: {
+    extensions: [".js"]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "manifest.json", to: "manifest.json" },
+        { from: "src/popup.html", to: "popup.html" },
+        { from: "src/options.html", to: "options.html" }
+      ]
+    })
+  ]
+};
