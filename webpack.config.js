c02ed1d9afb5048aea4e79970f5408a11c4619c8
Max <pearcebasmanm@gmail.com>
2025-02-01 14:23:32 -0500

    remove inlined conflict warnings

--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,7 @@ module.exports = {
     content: "./src/content.js",
     options: "./src/options.js",
     popup: "./src/popup.js",
-<<<<<<< HEAD
     notification: "./src/components/notification.js",
-=======
->>>>>>> 8ad9502cef4b3afba1e1a188488127f457937b19
   },
   output: {
     filename: "[name].js", // Produces dist/background.js, dist/content.js, etc.
@@ -26,10 +23,7 @@ module.exports = {
         { from: "manifest.json", to: "manifest.json" },
         { from: "src/popup.html", to: "popup.html" },
         { from: "src/options.html", to: "options.html" },
-<<<<<<< HEAD
-=======
         { from: "src/options.css", to: "options.css" },
->>>>>>> 8ad9502cef4b3afba1e1a188488127f457937b19
       ],
     }),
   ],
